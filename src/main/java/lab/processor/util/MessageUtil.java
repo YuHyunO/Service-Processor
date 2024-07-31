fc6e00c77b23f3fba96595657341c3c14ed64e30
YuHyunO <wwdg0913@gmail.com>
2024-07-31 17:58:27 +0900

    Add MessageUtil

--- src/main/java/lab/processor/util/MessageUtil.java
+++ src/main/java/lab/processor/util/MessageUtil.java
@@ -1,7 +1,15 @@
 package lab.processor.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Map;
 
 public class MessageUtil {
 
@@ -18,5 +26,35 @@ public class MessageUtil {
         return toStringBuf(throwable).toString();
     }
 
+    public static String mapToJson(Map map, boolean prettyFormat) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        String jsonString = objectMapper.writeValueAsString(map);
+
+        if (prettyFormat) {
+            objectMapper = new ObjectMapper();
+            Object jsonObject = objectMapper.readValue(jsonString, Object.class);
+            return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonObject);
+        }
+        return jsonString;
+    }
+
+    public static Map<String, Object> jsonToMap(String jsonData) throws JsonProcessingException{
+        if (jsonData == null)
+            return null;
+        ObjectMapper objectMapper = new ObjectMapper();
+        TypeReference<Map<String, Object>> typeReference = new TypeReference<Map<String,Object>>() {};
+        return objectMapper.readValue(jsonData, typeReference);
+    }
+
+    public static String mapToXml (Map map, boolean prettyFormat) throws JsonProcessingException {
+        XmlMapper xmlMapper = XmlMapper.builder().build();
+        if (prettyFormat) {
+            xmlMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        }
+        //modifying to not set class name for root node name
+        String xmlString = xmlMapper.writeValueAsString(map);
+        return xmlString;
+    }
 
 }
